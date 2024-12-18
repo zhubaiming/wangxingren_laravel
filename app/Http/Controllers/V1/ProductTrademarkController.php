0877970e84a65a5f42767dbf79541022886dbe6c
zhubaiming <other.zhubaiming@gmail.com>
2024-12-18 12:50:41 +0000

    商品品牌

--- app/Http/Controllers/V1/ProductTrademarkController.php
+++ app/Http/Controllers/V1/ProductTrademarkController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductTrademarkResource;
 use App\Models\ProductTrademark;
 use App\Services\ProductTrademarkService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ProductTrademarkController extends Controller
@@ -20,8 +22,93 @@ class ProductTrademarkController extends Controller
 
         $query = ProductTrademark::orderBy('letter', 'asc');
 
+        if ($request->has('title') && !is_null($request->get('title'))) {
+            $query = $query->where('title', 'like', "%{$request->get('title')}%");
+        }
+
         $payload = $paginate ? $query->paginate($request->get('pageSize') ?? $this->pageSize, ['*'], 'page', $request->get('page') ?? $this->page) : $query->get();
 
         return $this->returnIndex($payload, 'ProductTrademarkResource', __FUNCTION__, $paginate);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = arrHumpToLine($request->post());
+
+        if (0 === ProductTrademark::where(['title' => $validate['title']])->count('id')) {
+
+            $trademark = ProductTrademark::create(['title' => $validate['title'], 'letter' => isset($validate['letter']) ? strtolower($validate['letter']) : null, 'image' => $validate['image'] ?? null, 'description' => $validate['description'] ?? null]);
+
+            if (!$trademark) {
+                return $this->failed('品牌创建失败');
+            }
+
+            return $this->message('success');
+        }
+
+        return $this->failed('当前品牌已存在，请重新建立');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $payload = ProductTrademark::findOrFail($id);
+
+            return $this->success((new ProductTrademarkResource($payload))->additional(['format' => __FUNCTION__]));
+        } catch (ModelNotFoundException $e) {
+            return $this->failed('未找到数据');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validate = arrHumpToLine($request->post());
+
+        if (0 === ProductTrademark::where(['title' => $validate['title']])->whereNot('id', $id)->count('id')) {
+
+            try {
+                $trademark = ProductTrademark::findOrFail($id);
+
+                foreach ($validate as $field => $value) {
+                    if ($field !== 'id') {
+                        $trademark->setAttribute($field, $value);
+                    }
+                }
+
+                $trademark->save();
+
+                return $this->message('success');
+            } catch (ModelNotFoundException) {
+                return $this->failed('要修改的品牌不存在');
+            }
+        }
+
+        return $this->failed('当前品牌已存在，请重新建立');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        ProductTrademark::destroy($id);
+
+        return $this->message('success');
+    }
+
+    public function batchDestroy(Request $request)
+    {
+        ProductTrademark::destroy($request->post('ids'));
+
+        return $this->message('success');
     }
 }
