acce72b26c6804b38336ccbde2f4f4e4d1b49eac
zhubaiming <other.zhubaiming@gmail.com>
2025-01-24 18:44:31 +0000

    添加后台显示订单详细信息

--- app/Http/Controllers/Api/Admin/ClientUser/OrderController.php
+++ app/Http/Controllers/Api/Admin/ClientUser/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Admin\ClientUser;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ClientUserOrderResource;
 use App\Models\ClientUserOrder;
 use Illuminate\Http\Request;
 
@@ -30,15 +31,15 @@ class OrderController extends Controller
             return $query->where('trade_no', $validated['trade_no']);
         })->when(isset($validated['pay_channel']), function ($query) use ($validated) {
             return $query->where('pay_channel', $validated['pay_channel']);
-        })->when(isset($validated['status']), function ($query) use ($validated) {
-            return $query->where('status', $validated['status']);
+        })->when(isset($validated['order_status']), function ($query) use ($validated) {
+            return $query->where('status', $validated['order_status']);
         })->when(isset($validated['person_phone_number']), function ($query) use ($validated) {
             return $query->where('address_json->person_phone_number', 'like', '%' . $validated['pay_channel'] . '%');
         })->when(isset($validated['address']), function ($query) use ($validated) {
             return $query->where('address_json->full_address', 'like', '%' . $validated['address'] . '%');
         })->orderBy('created_at','desc');
 
-        $payload = $paginate ? $query->paginate($request->get('pageSize') ?? $this->pageSize, ['*'], 'page', $request->get('page') ?? $this->page) : $query->get();
+        $payload = $paginate ? $query->paginate($request->get('page_size') ?? $this->pageSize, ['*'], 'page', $request->get('page') ?? $this->page) : $query->get();
 
         return $this->returnIndex($payload, 'ClientUserOrderResource', __FUNCTION__, $paginate);
     }
@@ -56,7 +57,9 @@ class OrderController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $payload = ClientUserOrder::findOrFail($id);
+
+        return $this->success((new ClientUserOrderResource($payload))->additional(['format' => __FUNCTION__]));
     }
 
     /**
