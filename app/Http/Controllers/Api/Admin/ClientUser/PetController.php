1905c037330426f8cbea935bd76920aef16cfa91
zhubaiming <other.zhubaiming@gmail.com>
2025-02-11 08:04:05 +0000

    添加后台创建客户宠物

--- app/Http/Controllers/Api/Admin/ClientUser/PetController.php
+++ app/Http/Controllers/Api/Admin/ClientUser/PetController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\Admin\ClientUser;
 use App\Http\Controllers\Controller;
 use App\Models\ClientUserPet;
 use App\Models\ProductSpu;
+use App\Models\SysPetBreed;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
@@ -18,7 +20,7 @@ class PetController extends Controller
         $paginate = isset($validated['paginate']) ? isTrue($validated['paginate']) : true; // 是否分页
 
         $breeds = isset($validated['spu_id']) ? ProductSpu::with('spu_breed')->findOrFail($validated['spu_id'])->spu_breed->pluck('id')->toArray() : [];
-        
+
         $query = ClientUserPet::where('user_id', $validated['user_id'])
             ->when(isset($validated['spu_id']), function ($breed) use ($breeds) {
                 $breed->whereIn('breed_id', $breeds);
@@ -35,7 +37,27 @@ class PetController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = arrHumpToLine($request->input());
+
+        $petBreed = SysPetBreed::select('title')->findOrFail($validated['breed_id']);
+
+        ClientUserPet::create([
+            'user_id' => $validated['user_id'],
+            'breed_id' => $validated['breed_id'],
+            'breed_title' => $petBreed->title,
+            'name' => $validated['name'],
+            'breed_type' => $validated['breed_type'],
+            'gender' => $validated['gender'],
+            'weight' => $validated['weight'],
+            'color' => $validated['color'] ?? null,
+            'avatar' => null,
+            'remark' => $validated['remark'] ?? null,
+            'is_sterilization' => isTrue($validated['is_sterilization']) ?? false,
+            'is_default' => false,
+            'birth' => Carbon::createFromTimeStamp($validated['birth'] / 1000, config('app.timezone'))->format('Y-m'),
+        ]);
+
+        return $this->success();
     }
 
     /**
