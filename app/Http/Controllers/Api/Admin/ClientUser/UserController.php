802d843d3bcc05ffeec40b4c353f7d0897d8eddb
zhubaiming <other.zhubaiming@gmail.com>
2025-02-11 04:42:09 +0000

    后台客户列表添加筛选

--- app/Http/Controllers/Api/Admin/ClientUser/UserController.php
+++ app/Http/Controllers/Api/Admin/ClientUser/UserController.php
@@ -16,11 +16,26 @@ class UserController extends Controller
         $validated = arrHumpToLine($request->input());
         $paginate = isset($validated['paginate']) ? isTrue($validated['paginate']) : true; // 是否分页
 
-        $query = ClientUser::where('is_freeze', false)->orderBy('id', 'asc');
+//        $query = ClientUser::where('is_freeze', false)->orderBy('id', 'asc');
 
-        $payload = $paginate ? $query->paginate($request->get('pageSize') ?? $this->pageSize, ['*'], 'page', $request->get('page') ?? $this->page) : $query->get();
+        $payload = ClientUser::when(isset($validated['name']), function ($query) use ($validated) {
+            $query->where('name', 'like', '%' . $validated['name'] . '%');
+        })
+            ->when(isset($validated['phone']), function ($query) use ($validated) {
+                $query->where('phone_number', 'like', '%' . $validated['phone'] . '%');
+            })
+            ->when(isset($validated['gender']), function ($query) use ($validated) {
+                $query->where('gender', $validated['gender']);
+            })
+//            ->when(isset($validated['status']), function ($query) use ($validated) {
+//                $query->where('gender', $validated['gender']);
+//            })
+            ->orderBy('id', 'asc');
 
-        return $this->returnIndex($payload, 'ClientUserResource', __FUNCTION__, $paginate);
+//        $payload = $paginate ? $payload->paginate($request->get('page_size') ?? $this->pageSize, ['*'], 'page', $request->get('page') ?? $this->page) : $query->get();
+        $payload = $paginate ? $payload->paginate($validated['page_size'] ?? $this->pageSize, ['*'], 'page', $validated['page'] ?? $this->page) : $paginate->get();
+
+        return $this->success($this->returnIndex($payload, 'ClientUserResource', __FUNCTION__, $paginate));
     }
 
     /**
