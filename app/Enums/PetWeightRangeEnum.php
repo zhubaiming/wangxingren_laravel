c311e63883f02f71b5d548517a4dd38d5cd14b31
zhubaiming <other.zhubaiming@gmail.com>
2025-02-11 03:48:09 +0000

    删除空格格式

--- app/Enums/PetWeightRangeEnum.php
+++ app/Enums/PetWeightRangeEnum.php
@@ -20,17 +20,17 @@ enum PetWeightRangeEnum: int implements CommonEnum
     public function name(string $type = null)
     {
         return match ($this) {
-            self::FIVE => ' 0-5公斤',
+            self::FIVE => '0-5公斤',
             self::EIGHT => '5-8公斤',
             self::ELEVEN => '8-11公斤',
-            self::SIXTEEN => ' 11-16公斤',
-            self::TWENTY_ONE => ' 16-21公斤',
-            self::TWENTY_SIX => ' 21-26公斤',
-            self::THIRTY => ' 26-30公斤',
-            self::FORTY => ' 30-40公斤',
-            self::SIXTY => ' 40-60公斤',
-            self::EIGHTY => ' 60-80公斤',
-            self::HUNDRED => ' 80-100公斤'
+            self::SIXTEEN => '11-16公斤',
+            self::TWENTY_ONE => '16-21公斤',
+            self::TWENTY_SIX => '21-26公斤',
+            self::THIRTY => '26-30公斤',
+            self::FORTY => '30-40公斤',
+            self::SIXTY => '40-60公斤',
+            self::EIGHTY => '60-80公斤',
+            self::HUNDRED => '80-100公斤'
         };
     }
 }
