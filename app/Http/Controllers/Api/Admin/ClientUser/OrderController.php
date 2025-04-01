95dd9e7aa978c1cb2839c88f470c6fffa7448b2a
zhubaiming <other.zhubaiming@gmail.com>
2025-04-01 15:06:26 +0800

    调整后台预约锁定时间redis键格式

--- app/Http/Controllers/Api/Admin/ClientUser/OrderController.php
+++ app/Http/Controllers/Api/Admin/ClientUser/OrderController.php
@@ -149,7 +149,7 @@ class OrderController extends Controller
 
         if (max($payer_total, 0) !== 0 && !in_array($pay_channel, PayChannelEnum::getOffLineChannels())) {
             // 入库前锁定时间
-            Redis::connection('order')->rpush('reservation_date_' . $reservation_date . '-' . $reservation[0], json_encode([
+            Redis::connection('order')->rpush('reservation_date_' . $reservation_date->format('Y-m-d') . '-' . $reservation[0], json_encode([
                 'start' => $reservation[1],
                 'end' => $reservation[2],
             ], JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
